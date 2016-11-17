b10705ecbded812c32912a53cf16a1e824ba86af
josepapaianni <jose.papaianni@mercadolibre.com>
2016-11-17 09:44:46 -0300

    add config for production version

--- config/webpack.production.js
+++ config/webpack.production.js
@@ -5,6 +5,10 @@ const WebpackShellPlugin = require('webpack-shell-plugin');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
+<<<<<<< 3112ec53c2ea63336881dab6b9d3121b9c9240c6
+=======
+  target: 'node',
+>>>>>>> production config
   entry: [
     './src/head-sync.js'
   ],
@@ -35,6 +39,7 @@ module.exports = {
         'NODE_ENV': JSON.stringify('production')
       }
     }),
+<<<<<<< 3112ec53c2ea63336881dab6b9d3121b9c9240c6
     new webpack.optimize.UglifyJsPlugin({
       debug: false,
       minimize: true,
@@ -49,5 +54,21 @@ module.exports = {
     library: 'head-sync',
     path: path.resolve() + '/lib',
     filename: 'head-sync.js'
+=======
+    // new webpack.optimize.UglifyJsPlugin({
+    //   debug: false,
+    //   minimize: true,
+    //   sourcemap: false,
+    //   output: {
+    //     comments: false
+    //   }
+    // }),
+  ],
+  output: {
+    libraryTarget: 'umd',
+    library: 'HeadSync',
+    path: path.resolve() + '/dist',
+    filename: 'react-declarative-head.js'
+>>>>>>> production config
   }
 };
