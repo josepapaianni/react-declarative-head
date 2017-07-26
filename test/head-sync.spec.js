69a5eb3d70bafbde531684ee45da8e389c0974ec
Jose <jpapaianni@gmail.com>
2017-07-25 23:05:38 -0300

    add peek test

--- test/head-sync.spec.js
+++ test/head-sync.spec.js
@@ -5,15 +5,24 @@ import { shallow } from 'enzyme';
 
 import Head from '../src/head-sync';
 
-describe('React Declarative Head', () => {
+describe('React Declarative Head (server side)', () => {
+  beforeEach(() => {
+
+  });
+  
   it('has a displayName', function () {
     const el = <Head />;
     expect(el.type.displayName).to.be.a('string');
     expect(el.type.displayName).to.equal('SideEffect(Head)');
   });
 
-  it("it doesn't create an element in React Virtual DOM", () => {
+  it("doesn't create an element in React Virtual DOM", () => {
     const el = <div><Head><title>A Title</title></Head></div>;
     expect(ReactDOMServer.renderToStaticMarkup(el)).to.equal('<div></div>');
-  })
+  });
+
+  it("calling peek() creates a string with tags to put inside head", () => {
+    const el = <div><Head><title>A Title</title></Head></div>;
+    expect(Head.peek()).to.equal('<title>A Title</title>');
+  });
 });
